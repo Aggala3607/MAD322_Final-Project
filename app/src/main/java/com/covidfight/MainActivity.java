e44df10d832b6d6d8d34c0f0de6ea8254b65900e
Aggala <aggalashashank3607@gmail.com>
2021-06-27 12:04:38 -0400

    First Commit

---
+++ app/src/main/java/com/covidfight/MainActivity.java
@@ -0,0 +1,38 @@
+package com.covidfight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final int ScreenDisplay = 2000;
+        Thread t1=new Thread(){
+            int wait1=0;
+            public void run(){
+                try{
+                    while(wait1<=ScreenDisplay )
+                    {
+                        sleep(100);
+                        wait1+=100;
+                    }
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+                finally{
+                   // Intent i= new Intent(MainActivity.this, UserLoginActivity.class);
+                   // startActivity(i);
+                    finish();
+
+                }
+            }
+        };
+        t1.start();
+    }
+}
