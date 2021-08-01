a47621a86e3191717cb05885fcc728b75b5f84c1
bala <65573793+Balakrishna7777@users.noreply.github.com>
2021-08-01 11:59:49 -0400

    Register activity commit

--- app/src/main/java/com/covidfight/activity/AdminLoginActivity.java
+++ app/src/main/java/com/covidfight/activity/AdminLoginActivity.java
@@ -2,44 +2,104 @@ package com.covidfight.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.covidfight.R;
+import com.covidfight.Utils;
+import com.covidfight.api.ApiService;
+import com.covidfight.api.RetroClient;
+import com.covidfight.model.ResponseData;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AdminLoginActivity extends AppCompatActivity {
-
     EditText et_email, et_password;
     Button login_btn;
-
+    TextView admin_forget;
+    ProgressDialog p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
 
-        et_email =  findViewById(R.id.et_email);
-        et_password = findViewById(R.id.et_password);
-        login_btn = findViewById(R.id.login_btn);
+        getSupportActionBar().setTitle("Admin Login");
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        et_email = (EditText) findViewById(R.id.et_email);
+        et_password = (EditText) findViewById(R.id.et_password);
 
+        admin_forget = (TextView) findViewById(R.id.admin_forget);
+
+        login_btn = (Button) findViewById(R.id.login_btn);
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 if(et_email.getText().toString().isEmpty()){
-                    Toast.makeText(AdminLoginActivity.this, "Please Enter Username..", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(AdminLoginActivity.this, "Enter Email", Toast.LENGTH_SHORT).show();
                     return;
                 }
                 if(et_password.getText().toString().isEmpty()){
-                    Toast.makeText(AdminLoginActivity.this, "Please Enter Password..", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(AdminLoginActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
                     return;
                 }
-
-
+                adminLoginFunction();
             }
         });
+    }
+
+
+    public  void adminLoginFunction() {
+        p= new ProgressDialog(AdminLoginActivity.this);
+        p.setTitle("Login checking...");
+        //  p.show();
+        ApiService apiService = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<ResponseData> call = apiService.adminlogin(et_email.getText().toString(),et_password.getText().toString());
+
+        call.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                //    p.dismiss();
+                if (response.body().status.equals("true")) {
+                    SharedPreferences sharedPreferences = getSharedPreferences(Utils.SHREF, Context.MODE_PRIVATE);
+                    SharedPreferences.Editor et=sharedPreferences.edit();
+                    et.putString("user_name",et_email.getText().toString());
+                    et.commit();
+                    startActivity(new Intent(AdminLoginActivity.this, AdminDashboardActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(AdminLoginActivity.this, response.body().message, Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+                p.dismiss();
+                Toast.makeText(AdminLoginActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/covidfight/activity/AdminQuarantineGuidelines.java
+++ app/src/main/java/com/covidfight/activity/AdminQuarantineGuidelines.java
@@ -2,17 +2,35 @@ package com.covidfight.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.covidfight.R;
+import com.covidfight.api.ApiService;
+import com.covidfight.api.RetroClient;
+import com.covidfight.model.AdminInfoAdapter;
+import com.covidfight.model.Info;
+import com.covidfight.model.InfoAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AdminQuarantineGuidelines extends AppCompatActivity {
 
+    List<Info> info;
     Button btnaddquan;
+    ListView quanantine_list;
+    ProgressDialog p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +41,43 @@ public class AdminQuarantineGuidelines extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        quanantine_list=(ListView)findViewById(R.id.quanantine_list);
+
+
         btnaddquan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i=new Intent(getApplicationContext(),AdminAddQuarantine.class);
                 startActivity(i);
+            }
+        });
+        info = new ArrayList<>();
+        getQuarantineGuidelines();
+    }
+
+    public void getQuarantineGuidelines() {
+        p = new ProgressDialog(AdminQuarantineGuidelines.this);
+        p.setMessage("Loading....");
+        p.show();
+
+        ApiService service = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<List<Info>> call = service.getquarantineinfo();
+        call.enqueue(new Callback<List<Info>>() {
+            @Override
+            public void onResponse(Call<List<Info>> call, Response<List<Info>> response) {
+                p.dismiss();
+                //  Toast.makeText(AdminQuarantineGuidelines.this, response.toString(), Toast.LENGTH_SHORT).show();
+                if (response.body() == null) {
+                    Toast.makeText(AdminQuarantineGuidelines.this, "No data found", Toast.LENGTH_SHORT).show();
+                } else {
+                    info = response.body();
+                    quanantine_list.setAdapter(new AdminInfoAdapter(info, AdminQuarantineGuidelines.this));
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Info>> call, Throwable t) {
+                p.dismiss();
+                Toast.makeText(AdminQuarantineGuidelines.this, "Please contact admin !", Toast.LENGTH_SHORT).show();
             }
         });
     }
