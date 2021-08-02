73ad610a1451aa72ff910f82225df472e6458908
bala <65573793+Balakrishna7777@users.noreply.github.com>
2021-08-02 01:49:55 -0400

    updated adminAddNews

--- app/src/main/java/com/covidfight/activity/adminAddNews.java
+++ app/src/main/java/com/covidfight/activity/adminAddNews.java
@@ -1,17 +1,52 @@
 package com.covidfight.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.covidfight.R;
+import com.covidfight.api.ApiService;
+import com.covidfight.api.RetroClient;
+import com.covidfight.model.ResponseData;
 
-public class adminAddNews extends AppCompatActivity {
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import pub.devrel.easypermissions.EasyPermissions;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class adminAddNews extends AppCompatActivity implements EasyPermissions.PermissionCallbacks{
+    ProgressDialog p;
     EditText add_news_title, add_news_description;
-    Button btn_add_news;
+    Button btn_add_news,btn_uploadpic;
+    private static final int REQUEST_GALLERY_CODE = 200;
+    private static final int READ_REQUEST_CODE = 300;
+    private static final String TAG = adminAddNews.class.getSimpleName();
+    private static final String SERVER_PATH = "http://covidinformation.live/";
+    private Uri uri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +55,118 @@ public class adminAddNews extends AppCompatActivity {
         add_news_title = findViewById(R.id.add_news_title);
         add_news_description = findViewById(R.id.add_news_desc);
         btn_add_news = findViewById(R.id.btn_add_news);
+        btn_add_news.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(add_news_title.getText().toString().isEmpty()){
+                    Toast.makeText(adminAddNews.this, "Enter Title", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else if(add_news_description.getText().toString().isEmpty()){
+                    Toast.makeText(adminAddNews.this, "Enter Description", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                else {
+                    addNews();
+                }
+
+            }
+        });
+        btn_uploadpic=(Button) findViewById(R.id.btn_add_image);
+        btn_uploadpic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK);
+                openGalleryIntent.setType("image/*");
+                startActivityForResult(openGalleryIntent, REQUEST_GALLERY_CODE);
+            }
+        });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, adminAddNews.this);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_GALLERY_CODE && resultCode == Activity.RESULT_OK){
+            uri = data.getData();
+            if(EasyPermissions.hasPermissions(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                String filePath = getRealPathFromURIPath(uri, adminAddNews.this);
+                file = new File(filePath);
+
+            }else{
+                EasyPermissions.requestPermissions(this, getString(R.string.read_file), READ_REQUEST_CODE, Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+        }
+    }
+    private String getRealPathFromURIPath(Uri contentURI, Activity activity) {
+        Cursor cursor = activity.getContentResolver().query(contentURI, null, null, null, null);
+        if (cursor == null) {
+            return contentURI.getPath();
+        } else {
+            cursor.moveToFirst();
+            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+            return cursor.getString(idx);
+        }
+    }
+    File file;
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+        if(uri != null){
+            String filePath = getRealPathFromURIPath(uri, adminAddNews.this);
+            file = new File(filePath);
+        }
+    }
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        Log.d(TAG, "Permission has been denied");
+
+    }
+
+    private void addNews() {
+        p = new ProgressDialog(adminAddNews.this);
+        p.setTitle("Loading");
+        p.show();
+        Map<String, String> map = new HashMap<>();
+        map.put("title", add_news_title.getText().toString());
+        map.put("description", add_news_description.getText().toString());
+        RequestBody mFile = RequestBody.create(MediaType.parse("image/*"), file);
+        MultipartBody.Part fileToUpload = MultipartBody.Part.createFormData("file", file.getName(), mFile);
+        RequestBody filename = RequestBody.create(MediaType.parse("text/plain"), file.getName());
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(SERVER_PATH)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService service = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<ResponseData> call = service.news(fileToUpload, map);
+        call.enqueue(new Callback<ResponseData>() {
+            @Override
+            public void onResponse(Call<ResponseData> call, Response<ResponseData> response) {
+                p.dismiss();
+                Toast.makeText(adminAddNews.this, "Added successfully. ", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(adminAddNews.this,AdminDashboardActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseData> call, Throwable t) {
+                p.dismiss();
+                Toast.makeText(adminAddNews.this, "Error" + t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
