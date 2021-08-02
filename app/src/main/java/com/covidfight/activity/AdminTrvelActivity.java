01f4ad21d0be61ec9e2b076256e1ca1667d60e0c
bala <65573793+Balakrishna7777@users.noreply.github.com>
2021-08-02 01:58:25 -0400

    updated adminAddNews

--- app/src/main/java/com/covidfight/activity/AdminTrvelActivity.java
+++ app/src/main/java/com/covidfight/activity/AdminTrvelActivity.java
@@ -2,16 +2,36 @@ package com.covidfight.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.covidfight.R;
+import com.covidfight.api.ApiService;
+import com.covidfight.api.RetroClient;
+import com.covidfight.model.AdminInfoAdapter;
+import com.covidfight.model.Info;
+import com.covidfight.model.InfoAdapter;
+import com.covidfight.model.TravelAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AdminTrvelActivity extends AppCompatActivity {
 
+    List<Info> info;
+
+    ListView travel_list;
+    ProgressDialog p;
     Button btntravel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +41,43 @@ public class AdminTrvelActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        travel_list=(ListView)findViewById(R.id.travel_list);
         btntravel=(Button)findViewById(R.id.btntravel);
         btntravel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i=new Intent(getApplicationContext(),addTravelGuidelines.class);
                 startActivity(i);
+            }
+        });
+        info = new ArrayList<>();
+        getTravelGuidelines();
+    }
+
+
+    public void getTravelGuidelines() {
+        p = new ProgressDialog(AdminTrvelActivity.this);
+        p.setMessage("Loading....");
+        p.show();
+
+        ApiService service = RetroClient.getRetrofitInstance().create(ApiService.class);
+        Call<List<Info>> call = service.gettravelinfo();
+        call.enqueue(new Callback<List<Info>>() {
+            @Override
+            public void onResponse(Call<List<Info>> call, Response<List<Info>> response) {
+                p.dismiss();
+                // Toast.makeText(AdminTrvelActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                if (response.body() == null) {
+                    Toast.makeText(AdminTrvelActivity.this, "No data found", Toast.LENGTH_SHORT).show();
+                } else {
+                    info = response.body();
+                    travel_list.setAdapter(new TravelAdapter(info, AdminTrvelActivity.this));
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Info>> call, Throwable t) {
+                p.dismiss();
+                Toast.makeText(AdminTrvelActivity.this, "Please contact admin !", Toast.LENGTH_SHORT).show();
             }
         });
     }
